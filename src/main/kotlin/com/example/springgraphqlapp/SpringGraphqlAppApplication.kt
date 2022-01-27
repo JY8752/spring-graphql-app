9b51651f554e7d24498876c54624a7a4a5a2c70b
JY8752 <j.y.8752@gmail.com>
2022-01-27 21:10:10 +0900

    first commit

---
+++ src/main/kotlin/com/example/springgraphqlapp/SpringGraphqlAppApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springgraphqlapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringGraphqlAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringGraphqlAppApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springgraphqlapp/SpringGraphqlAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springgraphqlapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringGraphqlAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
